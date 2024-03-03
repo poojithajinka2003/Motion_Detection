901b23f876d070e57c39fc3cecd15bebeb6c9250
poojithajinka2003 <125656702+poojithajinka2003@users.noreply.github.com>
2024-03-03 15:02:57 +0530

    Delete motion_detection_project/templates/script.js

--- motion_detection_project/templates/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("startButton").addEventListener("click", function() {
-    fetch('/start_motion_detection')
-        .then(response => response.text())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error:', error));
-});
